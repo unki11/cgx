0853e2442f1bf71e66fb6681020379b002237562
yoonskk28 <bibleey@naver.com>
2020-02-04 14:43:53 +0900

    vo,mapper 제작

--- CGX_final_Project/src/main/java/com/kh/cgx/controller/CinemaController.java
+++ CGX_final_Project/src/main/java/com/kh/cgx/controller/CinemaController.java
@@ -1,16 +1,25 @@
 package com.kh.cgx.controller;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.cgx.entity.cinema.CinemaDto;
 
 @Controller
 @RequestMapping("/cinema")
 public class CinemaController {
 
+	@Autowired
+	private SqlSession sqlSession;
+	
 	@GetMapping("/")
 	public String cinema() {
 		
+//		CinemaDto dto = sqlSession.selectList("cinema.list");
+//		CinemaDto dto = sqlSession.selectList("cinema.list");
 		return "cinema/cinema";
 	}
 }
---
+++ CGX_final_Project/src/main/java/com/kh/cgx/entity/cinema/CinemaDto.java
@@ -0,0 +1,13 @@
+package com.kh.cgx.entity.cinema;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class CinemaDto {
+
+	private int cinema_no, file_no;
+	private String cinema_area, cinema_phone , cinema_addr;
+}
