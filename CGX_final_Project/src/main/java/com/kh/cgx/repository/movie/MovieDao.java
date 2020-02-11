c8419d781cee741f582e718fc5bd920ee897a4f1
82108 <82108@DESKTOP-KAAMH1N>
2020-02-11 19:03:47 +0900

    무비파인더 완료

--- CGX_final_Project/src/main/java/com/kh/cgx/repository/movie/MovieDao.java
+++ CGX_final_Project/src/main/java/com/kh/cgx/repository/movie/MovieDao.java
@@ -7,10 +7,13 @@ import com.kh.cgx.entity.movie.MovieDto;
 
 public interface MovieDao {
 
-	List<MovieDto> getList();
+	List<MovieDto> finder(String type,Object keyword);
 
+	int count(String type , Object keyword) ;
+	
 	List<MovieDto> getList2();
 	
+	List<MovieDto> getList3();
 
 	
 	
--- CGX_final_Project/src/main/java/com/kh/cgx/repository/movie/MovieDaoImpl.java
+++ CGX_final_Project/src/main/java/com/kh/cgx/repository/movie/MovieDaoImpl.java
@@ -1,6 +1,8 @@
 package com.kh.cgx.repository.movie;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +22,7 @@ public class MovieDaoImpl implements MovieDao{
 	private SqlSession sqlSession;
 	
 	@Override
-	public List<MovieDto> getList() {
+	public List<MovieDto> getList3() {
 		return sqlSession.selectList("movies.list");
 	}
 
@@ -28,6 +30,29 @@ public class MovieDaoImpl implements MovieDao{
 	public List<MovieDto> getList2() {
 		
 		return sqlSession.selectList("movies.pre_list");
+	}
+
+	//게시글 전체 목록 + 검색 기능
+	@Override
+	public List<MovieDto> finder(String type, Object keyword) {
+		//검색 옵션 , 키워드 맵에 저장
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("type", type);
+		param.put("keyword", keyword);
+		
+		return sqlSession.selectList("movies.finder_list", param);
+	}
+
+	//게시글 검색 결과 수
+	@Override
+	public int count(String type, Object keyword) {
+		//검색 옵션 , 키워드 맵에 저장
+		
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("type", type);
+		param.put("keyword", keyword);
+		
+		return sqlSession.selectOne("movies.count", param);
 	}
 
 	
