7fd94112693a30703435568a26175125dfdc8b2e
jun <jun@DESKTOP-MPBAH72>
2020-02-14 10:08:53 +0900

    매니저 상영시간표 입력 기능

--- CGX_final_Project/src/main/java/com/kh/cgx/repository/admin/ManagerDao.java
+++ CGX_final_Project/src/main/java/com/kh/cgx/repository/admin/ManagerDao.java
@@ -1,7 +1,16 @@
 package com.kh.cgx.repository.admin;
 
+import java.util.List;
+
 import com.kh.cgx.entity.admin.ManagerDto;
+import com.kh.cgx.entity.cinema.CinemaDto;
+import com.kh.cgx.entity.cinema.ScreenDto;
+import com.kh.cgx.entity.movie.MovieDto;
 
 public interface ManagerDao {
 	void insert(ManagerDto managerDto);
+	List<MovieDto> getMovieList();
+	List<ScreenDto> getScreenList(int no);
+	List<ManagerDto> getList();
+	List<CinemaDto> getCinemaList(int no);
 }
